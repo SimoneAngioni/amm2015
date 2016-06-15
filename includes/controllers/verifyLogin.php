7547b082442936f148646915de5e18d7ff37acd9
SimoneAngioni <simoangi94@libero.it>
2016-06-15 17:53:30 +0200

    changelog01

--- includes/controllers/verifyLogin.php
+++ includes/controllers/verifyLogin.php
@@ -5,16 +5,16 @@ $password = $_POST['psw'];
 $auth = new Connessione();
 $controllo = $auth->verifyLogin($nome,$password);
 	if($controllo == false){
-		header("Location: ../Views/homeErrore.php");
+		header("Location: ../views/homeErrore.php");
 	}
 	else
 	{
 		$_SESSION['autorizzato'] = 1;
 		$_SESSION['utente_id'] = $controllo['id_utentei'];
 		if($controllo['id_utenti'] == 1){
-			header("Location: ../Views/admin.php");
+			header("Location: ../views/admin.php");
 		}else if($controllo['id_utenti'] == 2){
-			header("Location: ../Views/utente.php");
+			header("Location: ../views/utente.php");
 		}	
 	}
 ?>
